d529e4d9784220e95348de7af637f4c0eef3c90f
Abhijit Pradhan <abhijitpradhan@Vision.local>
2014-12-16 16:30:55 -0600

    Updated styles for laundry organizer

--- app/views/my-laundry.blade.php
+++ app/views/my-laundry.blade.php
@@ -21,244 +21,355 @@
 @stop
 
 @section ('content')
-
-	<div class="col-md-10 col-md-offset-1">
-		<div class="col-md-12 cycle-heading"><h2>Wash Cycles</h2></div>
-			<div class="col-md-12">
-				<div class="cycle">
-				@if (isset($washData['mwNormal']))
-					<h3 class="cycle-heading">Machine Wash Normal</h3>
+	<div class="row">
+		<div class="col-md-offset-1 cycle-heading">
+			<h2>Wash Cycles</h2>
+		</div>
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-primary list-group">
+			@if (isset($washData['mwNormal']))
+				<div class="panel-heading">Machine Wash Normal</div>
 					@foreach ($washData['mwNormal'] as $value)
-						<div class="well">
-						{{ $value }}
+						<div class="list-group-item">
+							<div class="row-action-primary">
+								<i class="mdi-file-folder"></i>
+							</div>
+							<div class="row-content">
+								<div class="list-group-item-heading">{{ $value }}</div>
+							</div>
 						</div>
+						<div class="list-group-separator"></div>
 					@endforeach
-				@endif
-				</div>
-
-				<div class="cycle">
-				@if (isset($washData['mwCold']))
-					<h3 class="cycle-heading">Machine Wash Cold</h3>
-					@foreach ($washData['mwCold'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif	
-				</div>
-
-				<div class="cycle">
-				@if (isset($washData['mwWarm']))
-					<h3 class="cycle-heading">Machine Wash Warm</h3>
-					@foreach ($washData['mwWarm'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif			
-				</div>
-
-				<div class="cycle">
-				@if (isset($washData['mwHot']))
-					<h3 class="cycle-heading">Machine Wash Hot</h3>
-					@foreach ($washData['mwHot'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif
-				</div>
-
-				<div class="cycle">
-				@if (isset($washData['mwColdPerm']))
-					<h3 class="cycle-heading">Machine Wash Cold Permanent Press</h3>
-					@foreach ($washData['mwColdPerm'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif	
-				</div>
-
-				<div class="cycle">
-				@if (isset($washData['mwWarmPerm']))
-					<h3 class="cycle-heading">Machine Wash Warm Permanent Press</h3>
-					@foreach ($washData['mwWarmPerm'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif	
-				</div>
-
-				<div class="cycle">
-				@if (isset($washData['mwHotPerm']))
-					<h3 class="cycle-heading">Machine Wash Hot Permanent Press</h3>
-					@foreach ($washData['mwHotPerm'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif	
-				</div>
+			@endif
 			</div>
-
-		<div class="col-md-12 cycle-heading"><h2>Dry Cycles</h2></div>
-			<div class="col-md-12">
-				<div class="cycle">
-				@if (isset($dryData['tdNormal']))
-					<h3 class="cycle-heading">Tumble Dry, Normal</h3>
-					@foreach ($dryData['tdNormal'] as $value)
-						<div class="well">
-						{{ $value }}
+			<div class="panel panel-primary list-group">
+			@if (isset($washData['mwCold']))
+				<h3 class="panel-heading">Machine Wash Cold</h3>
+					@foreach ($washData['mwCold'] as $value)
+						<div class="list-group-item">
+							<div class="row-action-primary">
+								<i class="mdi-file-folder"></i>
+							</div>
+							<div class="row-content">
+								<div class="list-group-item-heading">{{ $value }}</div>
+							</div>
 						</div>
+						<div class="list-group-separator"></div>
 					@endforeach
-				@endif	
-				</div>
-
-				<div class="cycle">
-				@if (isset($dryData['tdNoHeat']))
-					<h3 class="cycle-heading">Tumble Dry, No Heat</h3>
-					@foreach ($dryData['tdNoHeat'] as $value)
-						<div class="well">
-						{{ $value }}
+			@endif	
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($washData['mwWarm']))
+				<h3 class="panel-heading">Machine Wash Warm</h3>
+					@foreach ($washData['mwWarm'] as $value)
+						<div class="list-group-item">
+							<div class="row-action-primary">
+								<i class="mdi-file-folder"></i>
+							</div>
+							<div class="row-content">
+								<div class="list-group-item-heading">{{ $value }}</div>
+							</div>
 						</div>
+						<div class="list-group-separator"></div>
 					@endforeach
-				@endif	
-				</div>
-
-				<div class="cycle">
-				@if (isset($dryData['tdLowHeat']))
-					<h3 class="cycle-heading">Tumble Dry, Low Heat</h3>
-					@foreach ($dryData['tdLowHeat'] as $value)
-						<div class="well">
-						{{ $value }}
+			@endif			
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($washData['mwHot']))
+				<h3 class="panel-heading">Machine Wash Hot</h3>
+					@foreach ($washData['mwHot'] as $value)
+						<div class="list-group-item">
+							<div class="row-action-primary">
+								<i class="mdi-file-folder"></i>
+							</div>
+							<div class="row-content">
+								<div class="list-group-item-heading">{{ $value }}</div>
+							</div>
 						</div>
+						<div class="list-group-separator"></div>
 					@endforeach
-				@endif	
-				</div>
-
-				<div class="cycle">
-				@if (isset($dryData['tdMedium']))
-					<h3 class="cycle-heading">Tumble Dry, Medium Heat</h3>
-					@foreach ($dryData['tdMedium'] as $value)
-						<div class="well">
-						{{ $value }}
+			@endif
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($washData['mwColdPerm']))
+				<h3 class="panel-heading">Machine Wash Cold Permanent Press</h3>
+					@foreach ($washData['mwColdPerm'] as $value)
+						<div class="list-group-item">
+							<div class="row-action-primary">
+								<i class="mdi-file-folder"></i>
+							</div>
+							<div class="row-content">
+								<div class="list-group-item-heading">{{ $value }}</div>
+							</div>
 						</div>
+						<div class="list-group-separator"></div>
 					@endforeach
-				@endif		
-				</div>
-
-				<div class="cycle">
-				@if (isset($dryData['tdHigh']))
-					<h3 class="cycle-heading">Tumble Dry, High Heat</h3>
-					@foreach ($dryData['tdHigh'] as $value)
-						<div class="well">
-						{{ $value }}
+			@endif	
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($washData['mwWarmPerm']))
+				<h3 class="panel-heading">Machine Wash Warm Permanent Press</h3>
+					@foreach ($washData['mwWarmPerm'] as $value)
+						<div class="list-group-item">
+							<div class="row-action-primary">
+								<i class="mdi-file-folder"></i>
+							</div>
+							<div class="row-content">
+								<div class="list-group-item-heading">{{ $value }}</div>
+							</div>
 						</div>
+						<div class="list-group-separator"></div>
 					@endforeach
-				@endif	
-				</div>			
-
-				<div class="cycle">
-				@if (isset($dryData['tdNoHeatPerm']))
-					<h3 class="cycle-heading">Tumble Dry, No Heat, Permanent Press</h3>
-					@foreach ($dryData['tdNoHeatPerm'] as $value)
-						<div class="well">
-						{{ $value }}
+			@endif	
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($washData['mwHotPerm']))
+				<h3 class="panel-heading">Machine Wash Hot Permanent Press</h3>
+					@foreach ($washData['mwHotPerm'] as $value)
+						<div class="list-group-item">
+							<div class="row-action-primary">
+								<i class="mdi-file-folder"></i>
+							</div>
+							<div class="row-content">
+								<div class="list-group-item-heading">{{ $value }}</div>
+							</div>
 						</div>
+						<div class="list-group-separator"></div>
 					@endforeach
-				@endif	
-				</div>
-
-				<div class="cycle">
-				@if (isset($dryData['tdLowHeatPerm']))
-					<h3 class="cycle-heading">Tumble Dry, Low Heat, Permanent Press</h3>
-					@foreach ($dryData['tdLowHeatPerm'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif	
-				</div>
-
-				<div class="cycle">
-				@if (isset($dryData['tdMediumPerm']))
-					<h3 class="cycle-heading">Tumble Dry, Medium Heat, Permanent Press</h3>
-					@foreach ($dryData['tdMediumPerm'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif	
-				</div>	
-
-				<div class="cycle">
-				@if (isset($dryData['tdNoHeatGentle']))
-					<h3 class="cycle-heading">Tumble Dry, No Heat, Gentle</h3>
-					@foreach ($dryData['tdNoHeatGentle'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif
-				</div>
-
-				<div class="cycle">
-				@if (isset($dryData['tdLowHeatGentle']))
-					<h3 class="cycle-heading">Tumble Dry, Low Heat, Gentle</h3>
-					@foreach ($dryData['tdLowHeatGentle'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif	
-				</div>	
-
-				<div class="cycle">
-				@if (isset($dryData['tdMediumGentle']))
-					<h3 class="cycle-heading">Tumble Dry, Medium Heat, Gentle</h3>
-					@foreach ($dryData['tdMediumGentle'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif
-				</div>
-
-				<div class="cycle">
-				@if (isset($dryData['lineDry']))
-					<h3 class="cycle-heading">Line Dry</h3>
-					@foreach ($dryData['lineDry'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif
-				</div>
-
-				<div class="cycle">
-				@if (isset($dryData['dripDry']))
-					<h3 class="cycle-heading">Drip Dry</h3>
-					@foreach ($dryData['dripDry'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif	
-				</div>
-
-				<div class="cycle">
-				@if (isset($dryData['dryFlat']))
-					<h3 class="cycle-heading">Dry Flat</h3>
-					@foreach ($dryData['dryFlat'] as $value)
-						<div class="well">
-						{{ $value }}
-						</div>
-					@endforeach
-				@endif	
-				</div>
-			</div>																															
+			@endif	
+			</div>
 		</div>
 	</div>
+<!-- DRY CYCLES --> 
+	<div class="row">
+		<div class="col-md-offset-1 cycle-heading">
+			<h2>Dry Cycles</h2>
+		</div>
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdNormal']))
+				<h3 class="panel-heading">Tumble Dry, Normal</h3>
+				@foreach ($dryData['tdNormal'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif	
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdNoHeat']))
+				<h3 class="panel-heading">Tumble Dry, No Heat</h3>
+				@foreach ($dryData['tdNoHeat'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif	
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdLowHeat']))
+				<h3 class="panel-heading">Tumble Dry, Low Heat</h3>
+				@foreach ($dryData['tdLowHeat'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif	
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdMedium']))
+				<h3 class="panel-heading">Tumble Dry, Medium Heat</h3>
+				@foreach ($dryData['tdMedium'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif		
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdHigh']))
+				<h3 class="panel-heading">Tumble Dry, High Heat</h3>
+				@foreach ($dryData['tdHigh'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif	
+			</div>			
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdNoHeatPerm']))
+				<h3 class="panel-heading">Tumble Dry, No Heat, Permanent Press</h3>
+				@foreach ($dryData['tdNoHeatPerm'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif	
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdLowHeatPerm']))
+				<h3 class="panel-heading">Tumble Dry, Low Heat, Permanent Press</h3>
+				@foreach ($dryData['tdLowHeatPerm'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif	
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdMediumPerm']))
+				<h3 class="panel-heading">Tumble Dry, Medium Heat, Permanent Press</h3>
+				@foreach ($dryData['tdMediumPerm'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif	
+			</div>	
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdNoHeatGentle']))
+				<h3 class="panel-heading">Tumble Dry, No Heat, Gentle</h3>
+				@foreach ($dryData['tdNoHeatGentle'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdLowHeatGentle']))
+				<h3 class="panel-heading">Tumble Dry, Low Heat, Gentle</h3>
+				@foreach ($dryData['tdLowHeatGentle'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif	
+			</div>	
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['tdMediumGentle']))
+				<h3 class="panel-heading">Tumble Dry, Medium Heat, Gentle</h3>
+				@foreach ($dryData['tdMediumGentle'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['lineDry']))
+				<h3 class="panel-heading">Line Dry</h3>
+				@foreach ($dryData['lineDry'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['dripDry']))
+				<h3 class="panel-heading">Drip Dry</h3>
+				@foreach ($dryData['dripDry'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif	
+			</div>
+			<div class="panel panel-primary list-group">
+			@if (isset($dryData['dryFlat']))
+				<h3 class="panel-heading">Dry Flat</h3>
+				@foreach ($dryData['dryFlat'] as $value)
+					<div class="list-group-item">
+						<div class="row-action-primary">
+							<i class="mdi-file-folder"></i>
+						</div>
+						<div class="row-content">
+							<div class="list-group-item-heading">{{ $value }}</div>
+						</div>
+					</div>
+					<div class="list-group-separator"></div>
+				@endforeach
+			@endif	
+			</div>
+	 	</div>
+ 	</div>
 @stop
