7b248d090a9a157abe78034b7a0303e3febba188
Abhijit Pradhan <abhijitpradhan@Vision.local>
2014-12-17 20:45:22 -0600

    added validation to Item Search

--- app/views/item_search_result.blade.php
+++ app/views/item_search_result.blade.php
@@ -1,29 +1,35 @@
 <section>
-	{{ Form::open(array('url' => '/sort-laundry', 'method' => 'POST', 'class' => 'form-horizontal')) }}
-		@foreach ($items as $item)
-			<div class="well col-md-6 col-md-offset-2">
+	@if (count($items) >= 1)
+		{{ Form::open(array('url' => '/sort-laundry', 'method' => 'POST', 'class' => 'form-horizontal')) }}
+			@foreach ($items as $item)
+				<div class="well col-md-6 col-md-offset-2">
 
-				<img class="swatch" src="{{ asset($item->color_url) }}">
-				
-				<div class="item">
-					<span class="item-name">{{ $item->name; }}</span>	
-					<br> 					
-					@foreach ( $item->tags as $tag)
-						<small class="tag">{{ $tag->name }}</small>
-					@endforeach
-				</div>
+					<img class="swatch" src="{{ asset($item->color_url) }}">
+					
+					<div class="item">
+						<span class="item-name">{{ $item->name; }}</span>	
+						<br> 					
+						@foreach ( $item->tags as $tag)
+							<small class="tag">{{ $tag->name }}</small>
+						@endforeach
+					</div>
 
-				<div class="modify-buttons">
-					<span class="checkbox item-checkbox">
-						<label>
-							{{ Form::checkbox($item->id, $item->name) }}
-						</label>
-					</span>						
+					<div class="modify-buttons">
+						<span class="checkbox item-checkbox">
+							<label>
+								{{ Form::checkbox($item->id, $item->name) }}
+							</label>
+						</span>						
+					</div>
 				</div>
-			</div>
-		@endforeach
-		{{ Form::submit('Sort!', array('class' => 'btn btn-primary btn-raised btn-lg sort')) }}
-	{{ Form::close() }}
+			@endforeach
+			{{ Form::submit('Sort!', array('class' => 'btn btn-primary btn-raised btn-lg sort')) }}
+		{{ Form::close() }}
+	@else
+		<div class="well col-md-6 col-md-offset-2">
+			<h4 class="not-found">Query Not Found!</h4>
+		</div>
+	@endif
     <script>
         $(document).ready(function() {
             $.material.init();
